6a3c12901380017428f36135614db54b7ddfa8a4
Felipe Plazas <felipeplazas@Felipes-MacBook-Pro-2.local>
2017-09-17 01:24:34 -0500

    Added new card and routing behaviour

--- client/src/App.js
+++ client/src/App.js
@@ -28,6 +28,31 @@ class PlayerStatsView extends React.Component {
         return (
 
             <div className="App">
+                <section>
+                    <div className="container py-3">
+                        <div className="card">
+                            <div className="row ">
+                                <div className="col-md-4">
+                                    <img src="https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97400&w=400&h=400"
+                                         className="w-100"></img>
+                                </div>
+                                <div className="col-md-8 px-3">
+                                    <div className="card-block px-3">
+                                        <h4 className="card-title">Lorem ipsum dolor sit amet</h4>
+                                        <p className="card-text">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                            laboris nisi ut aliquip ex ea commodo consequat. </p>
+                                        <p className="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                                            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                            culpa qui officia deserunt mollit anim id est laborum.</p>
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </section>
                 <div className="row text-center">
                     {this.state.users.map((user, i) =>
                         <AddPlayers key={i} name={user.name} url={user.url} points={user.points} age={user.age}/>
@@ -60,6 +85,11 @@ class PlayersView extends React.Component {
     render() {
         return (
             <div className="App">
+                <header className="jumbotron my-4" style={{marginBottom: 5+'px', paddingBottom: 5+'px'}}>
+                    <h1 className="display-3">TenniStats</h1>
+                    <p className="lead">Find the statistics of your favorite tennis players. Just search any player and get it's latest
+                        games statistics. You will get the information of all the tennis tournaments in 2016!</p>
+                </header>
                 <div className="row text-center">
                     {this.state.users.map((user, i) =>
                         <AddPlayers key={i} name={user.name} url={user.url} points={user.points} age={user.age}/>
