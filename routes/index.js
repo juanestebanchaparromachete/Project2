6491fb316395bbf30c3b28519395bb900298b843
Felipe Plazas <felipeplazas@Felipes-MacBook-Pro-2.local>
2017-09-16 21:00:03 -0500

    Added new base structure in html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// var db = MongoClient.connect("mongodb://admin:AdminAdmin@ds139954.mlab.com:39954/datosdeportivos");
+var players = [];
+MongoClient.connect("mongodb://admin:AdminAdmin@ds139954.mlab.com:39954/datosdeportivos", function(err, db) {
+    if(!err) {
+        console.log("We are connected");
+
+        router.get('/', function(req, res, next) {
+            res.render('index', { title: 'Express' });
+        });
+
+        router.get('/players', function(req, res) {
+            var col = db.collection('Players');
+            col.find({}).toArray(function (mongoError, ej) {
+                  res.send(ej);
+                }
+            );
+        });
+    }
 });
+/* GET home page. */
+
 
 module.exports = router;
